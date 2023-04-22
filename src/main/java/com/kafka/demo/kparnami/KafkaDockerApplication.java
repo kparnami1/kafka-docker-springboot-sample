9bcae8069198a1fe1ab9e76df7a4b0c7b2d24be9
kparnami1 <kunalparnami15@gmail.com>
2023-04-22 13:59:59 -0700

    Initial commit
    Initial commit

---
+++ src/main/java/com/kafka/demo/kparnami/KafkaDockerApplication.java
@@ -0,0 +1,15 @@
+package com.kafka.demo.kparnami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDockerApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDockerApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/kafka/demo/kparnami/KparnamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.demo.kparnami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KparnamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
